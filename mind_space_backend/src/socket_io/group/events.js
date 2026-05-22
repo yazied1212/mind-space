3da8c45fdbb9d856ae9f7ad800bb59ff150077db
ziad <zezokandeel128@gmail.com>
2026-05-22 05:50:52 +0300

    join request edit

--- mind_space_backend/src/socket_io/group/events.js
+++ mind_space_backend/src/socket_io/group/events.js
@@ -2,7 +2,7 @@ import { GroupMessages } from "../../db/models/group messages.js";
 import { GM } from "../../db/models/group_members.js";
 import { SG } from "../../db/models/support_group.js";
 import { messages } from "../../utils/index.js";
-
+import { JoinRequest } from "../../db/models/JoinRequest.js";
 export const sendGroupMessage = async (socket, data) => {
     try {
         
@@ -44,3 +44,67 @@ export const sendGroupMessage = async (socket, data) => {
     
   };
 ;
+
+export const requestJoinGroup = async (socket, data) => {
+    const { groupId } = data
+
+    try {
+        // check group exists
+        const group = await SG.findById(groupId)
+        if (!group) {
+            return socket.emit("error", {
+                message: "Group not found",
+                statusCode: 404
+            })
+        }
+
+        // check not already a member
+        const alreadyMember = await GM.findOne({
+            groupId,
+            usersId: socket.userId
+        })
+        if (alreadyMember) {
+            return socket.emit("error", {
+                message: "You are already a member of this group",
+                statusCode: 400
+            })
+        }
+
+        // check not already requested
+        const alreadyRequested = await JoinRequest.findOne({
+            groupId,
+            userId: socket.userId,
+            status: "pending"
+        })
+        if (alreadyRequested) {
+            return socket.emit("error", {
+                message: "You already have a pending request",
+                statusCode: 400
+            })
+        }
+
+        // create request in DB
+        const joinRequest = await JoinRequest.create({
+            groupId,
+            userId: socket.userId
+        })
+
+        // notify the user
+        socket.emit("joinRequestSent", {
+            message: "Your request has been sent",
+            data: joinRequest
+        })
+
+        // notify the admin
+        socket.to(group.adminId.toString()).emit("newJoinRequest", {
+            message: "New join request",
+            data: joinRequest
+        })
+
+    } catch (error) {
+        return socket.emit("error", {
+            message: error.message,
+            statusCode: 400
+        })
+    }
+}
--- mind_space_backend/src/socket_io/session/events.js
+++ mind_space_backend/src/socket_io/session/events.js
@@ -1,8 +1,5 @@
 import { Session } from "../../db/models/session.js"
 import { AppError, messages } from "../../utils/index.js"
-import { JoinRequest } from "../../db/models/joinRequest.js"
-import { SG } from "../../db/models/sg.js"
-import { GM } from "../../db/models/gm.js"
 
 
 export const joinSession=async(socket,data)=>{
@@ -69,66 +66,3 @@ export const joinSession=async(socket,data)=>{
     }
 
 
-export const requestJoinGroup = async (socket, data) => {
-    const { groupId } = data
-
-    try {
-        // check group exists
-        const group = await SG.findById(groupId)
-        if (!group) {
-            return socket.emit("error", {
-                message: "Group not found",
-                statusCode: 404
-            })
-        }
-
-        // check not already a member
-        const alreadyMember = await GM.findOne({
-            groupId,
-            usersId: socket.userId
-        })
-        if (alreadyMember) {
-            return socket.emit("error", {
-                message: "You are already a member of this group",
-                statusCode: 400
-            })
-        }
-
-        // check not already requested
-        const alreadyRequested = await JoinRequest.findOne({
-            groupId,
-            userId: socket.userId,
-            status: "pending"
-        })
-        if (alreadyRequested) {
-            return socket.emit("error", {
-                message: "You already have a pending request",
-                statusCode: 400
-            })
-        }
-
-        // create request in DB
-        const joinRequest = await JoinRequest.create({
-            groupId,
-            userId: socket.userId
-        })
-
-        // notify the user
-        socket.emit("joinRequestSent", {
-            message: "Your request has been sent",
-            data: joinRequest
-        })
-
-        // notify the admin
-        socket.to(group.adminId.toString()).emit("newJoinRequest", {
-            message: "New join request",
-            data: joinRequest
-        })
-
-    } catch (error) {
-        return socket.emit("error", {
-            message: error.message,
-            statusCode: 400
-        })
-    }
-}
