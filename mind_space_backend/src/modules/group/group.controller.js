ba0d912f6c0c252b84d5924e24f8c729e74b05fc
ziad <zezokandeel128@gmail.com>
2026-05-29 02:06:52 +0300

    get groups APi

--- mind_space_backend/src/modules/group/group.controller.js
+++ mind_space_backend/src/modules/group/group.controller.js
@@ -2,7 +2,7 @@ import { Router } from "express"
 import { isAuthenticate } from "../../middlewares/auth.js"
 import { asyncHandler } from "../../utils/error/async_handler.js"
 import { isValid } from "../../middlewares/isValid.js"
-import { createGroup, getGroupMembers, joinGroupRequest, leaveGroup, removeUserFromGroup, updateGroup } from "./group.service.js"
+import { createGroup, getGroupMembers, getGroups, joinGroupRequest, leaveGroup, removeUserFromGroup, updateGroup } from "./group.service.js"
 import { createGroupSchema, GroupSchema,  removeUserFromGroupSchema, updateGroupSchema } from "./group.validation.js"
 import { isAuthorized } from "../../middlewares/isAuthorized.js"
 import { roles } from "../../utils/index.js"
@@ -15,7 +15,7 @@ router.post("/leave/:groupId",isAuthenticate,isValid(GroupSchema),asyncHandler(l
 router.post("/create-group",isAuthenticate,isAuthorized(roles.admin),isValid(createGroupSchema), asyncHandler(createGroup))
 router.delete("/remove-user/:groupId/:userId",isAuthenticate,isAuthorized(roles.admin),
 isValid(removeUserFromGroupSchema),asyncHandler(removeUserFromGroup))
-
+router.get("/get-groups",isAuthenticate,asyncHandler(getGroups))
 router.get("/get-members/:groupId",isAuthenticate,isValid(GroupSchema),asyncHandler(getGroupMembers))
 router.put("/update-group/:groupId",isAuthenticate,isAuthorized(roles.admin)
 ,isValid(updateGroupSchema),asyncHandler(updateGroup))
