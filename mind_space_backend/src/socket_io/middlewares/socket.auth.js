1bcea3600d8a2316ff16b77e9af27f1c26b10189
yazied1212 <yaziedmahmoud1@gmail.com>
2026-06-13 23:24:05 +0300

    next

--- mind_space_backend/src/socket_io/middlewares/socket.auth.js
+++ mind_space_backend/src/socket_io/middlewares/socket.auth.js
@@ -1,83 +1,48 @@
-import { TokenModel } from "../../db/models/token.js";
-import { User } from "../../db/models/user.js";
-import { AppError, messages, verifyToken } from "../../utils/index.js";
-
 export const socketAuth = async (socket, next) => {
   try {
     const { authorization } = socket.handshake.auth;
     if (!authorization) {
-      return socket.emit("errorMessage",{
-        message:"token not found",
-        statusCode:404
-      })
+      return next(new Error("token not found")); // 👈 Use next(new Error(...))
     }
     if (!authorization.startsWith("dash")) {
-      return socket.emit("errorMessage",{
-        message:"invalid bearer key",
-        statusCode:400
-      })
+      return next(new Error("invalid bearer key"));
     }
     const token = authorization.split(" ")[1];
 
     const decoded = verifyToken({token});
     
-   const isBlacklisted = await TokenModel.findOne({
-    jti: decoded.jti,
-    });
+    const isBlacklisted = await TokenModel.findOne({ jti: decoded.jti });
     if (isBlacklisted) {
-     return socket.emit("errorMessage",{
-        message:"token is invalid (logged out)",
-        statusCode:401
-      })
+     return next(new Error("token is invalid (logged out)"));
     }
 
     const user = await User.findById(decoded.id);
-    
 
     if (!user) {
-      return socket.emit("errorMessage",{
-        message:messages.user.notFound,
-        statusCode:404
-      })
+      return next(new Error(messages.user.notFound));
     }
 
     if (user.changeCredentialsTime?.getTime() > decoded.iat * 1000) {
-   return socket.emit("errorMessage",{
-        message:"Token is Expired",
-        statusCode:401
-      })
-};
+       return next(new Error("Token is Expired"));
+    }
 
     if (user.isDeleted) {
-      return socket.emit("errorMessage",{
-        message:"account deactivated please login first",
-        statusCode:400
-      })
+      return next(new Error("account deactivated please login first"));
     }
     
     if (user.bannedUntil && user.bannedUntil > Date.now()) {
-        return socket.emit("errorMessage",{
-        message:"your account is temporarily banned",
-        statusCode:403
-      })
-      }
-      
+        return next(new Error("your account is temporarily banned"));
+    }
 
     if (user.deletedAt && user.deletedAt.getTime() > decoded.iat * 1000) {
-      return socket.emit("errorMessage",{
-        message:"token is destroyed",
-        statusCode:400
-      })
+      return next(new Error("token is destroyed"));
     }
     
-    //*req.decoded = decoded;*/
     socket.authUser = user;
-    socket.userId=user._id
-    return next();
+    socket.userId = user._id;
+    return next(); // ✅ Connection approved!
+    
   } catch (error) {
-   return socket.emit("errorMessage",{
-        message:error.message,
-        statusCode:400
-      })
+    return next(new Error(error.message));
   }
 };
