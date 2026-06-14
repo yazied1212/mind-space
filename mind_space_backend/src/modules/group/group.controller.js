1b8f24e4e9a886bf585890366eb182bfdb7a0f44
ziad <zezokandeel128@gmail.com>
2026-06-14 21:43:32 +0300

    Revert "feat: complete backend support groups real-time chat and admin approval APIs"
    This reverts commit 9fce3ff8c1ba447b9d22d666511370acdc12e862.

--- mind_space_backend/src/modules/group/group.controller.js
+++ mind_space_backend/src/modules/group/group.controller.js
@@ -2,7 +2,7 @@ import { Router } from "express"
 import { isAuthenticate } from "../../middlewares/auth.js"
 import { asyncHandler } from "../../utils/error/async_handler.js"
 import { isValid } from "../../middlewares/isValid.js"
-import { createGroup, deleteGroup, getGroupMembers, getGroups, joinGroupRequest, leaveGroup, removeUserFromGroup, updateGroup, getGroupMessages, getPendingRequests, handleJoinRequest } from "./group.service.js"
+import { createGroup, deleteGroup, getGroupMembers, getGroups, joinGroupRequest, leaveGroup, removeUserFromGroup, updateGroup } from "./group.service.js"
 import { createGroupSchema, GroupSchema,  removeUserFromGroupSchema, updateGroupSchema } from "./group.validation.js"
 import { isAuthorized } from "../../middlewares/isAuthorized.js"
 import { roles } from "../../utils/index.js"
@@ -10,6 +10,7 @@ import { roles } from "../../utils/index.js"
 const router = Router()
 
 router.post("/join/:groupId", isAuthenticate, isValid(GroupSchema), asyncHandler(joinGroupRequest))
+// Admin accept or reject join requests (API) -----> need to be implemented by socket.io for real-time updates to users
 router.post("/leave/:groupId",isAuthenticate,isValid(GroupSchema),asyncHandler(leaveGroup))
 router.post("/create-group",isAuthenticate,isAuthorized(roles.admin),isValid(createGroupSchema), asyncHandler(createGroup))
 router.delete("/remove-user/:groupId/:userId",isAuthenticate,isAuthorized(roles.admin),
@@ -20,11 +21,4 @@ router.put("/update-group/:groupId",isAuthenticate,isAuthorized(roles.admin)
 ,isValid(updateGroupSchema),asyncHandler(updateGroup))
 router.delete("/delete-group/:groupId",isAuthenticate,isAuthorized(roles.admin),isValid(GroupSchema),asyncHandler(deleteGroup))
 
-// Message history
-router.get("/messages/:groupId", isAuthenticate, isValid(GroupSchema), asyncHandler(getGroupMessages))
-
-// Admin handles join requests
-router.get("/pending-requests", isAuthenticate, isAuthorized(roles.admin), asyncHandler(getPendingRequests))
-router.post("/handle-request/:requestId", isAuthenticate, isAuthorized(roles.admin), asyncHandler(handleJoinRequest))
-
-export default router
+export default router
--- mind_space_backend/src/socket_io/group/events.js
+++ mind_space_backend/src/socket_io/group/events.js
@@ -3,40 +3,9 @@ import { GM } from "../../db/models/group_members.js";
 import { SG } from "../../db/models/support_group.js";
 import { messages } from "../../utils/index.js";
 import { JoinRequest } from "../../db/models/JoinRequest.js";
-
-export const joinGroupRoom = async (socket, data) => {
-    const { groupId } = data;
-
-    const group = await SG.findById(groupId);
-    if (!group) {
-        return socket.emit("errorMessage", {
-            message: "Group not found",
-            statusCode: 404
-        });
-    }
-
-    const isMember = await GM.findOne({ groupId, usersId: socket.userId });
-    const isAdmin = group.adminId.toString() === socket.userId.toString();
-
-    if (!isMember && !isAdmin) {
-        return socket.emit("errorMessage", {
-            message: "You are not a member of this group",
-            statusCode: 403
-        });
-    }
-
-    socket.join(groupId);
-    socket.emit("joinedGroupRoom", { groupId, message: "Joined group room successfully" });
-};
-
-export const leaveGroupRoom = async (socket, data) => {
-    const { groupId } = data;
-    socket.leave(groupId);
-    socket.emit("leftGroupRoom", { groupId, message: "Left group room successfully" });
-};
-
 export const sendGroupMessage = async (socket, data) => {
-    const { groupId, message } = data;
+    
+    const {groupId, message} = data;
 
     if (!message?.trim()) {
       return socket.emit("errorMessage", {
@@ -52,92 +21,76 @@ export const sendGroupMessage = async (socket, data) => {
         })
     }
 
-    const isMember = await GM.findOne({ groupId, usersId: socket.userId });
-    const isAdmin = group.adminId.toString() === socket.userId.toString();
-
-    if (!isMember && !isAdmin) {
+    const isParticipant=await GM.findOne({groupId:groupId,usersId:socket.userId})
+    if(!isParticipant){
         return socket.emit("errorMessage",{
             message:"not allowed",
             statusCode:401
         })
     }
-
-    const messageData = {
-        sender: {
-            _id: socket.userId,
-            userName: socket.authUser.userName,
-            pfp: socket.authUser.pfp
-        },
-        message: message.trim(),
-        createdAt: new Date()
-    };
-
-    // Broadcast to others in the group room
-    socket.to(groupId).emit("receiveGroupMessage", {
-        groupId,
-        data: messageData
-    });
-
-    // Save to database (upsert if not exists)
-    await GroupMessages.findOneAndUpdate(
-      { groupId: groupId },
-      { $push: { messages: { sender: socket.userId, message: message.trim() } } },
-      { upsert: true, new: true }
-    );
-
-    // Confirm to sender
-    socket.emit("groupMessageSent", {
-        groupId,
-        data: messageData
-    });
-};
+    socket.to(groupId).emit("sendGroupMessage", {data: message });
+      await GroupMessages.findOneAndUpdate(
+        {groupId:groupId},
+        { $push: { messages: { sender: socket.userId, message } } },
+      );
+    } 
+;
 
 export const requestJoinGroup = async (socket, data) => {
-    const { groupId } = data;
+    const { groupId } = data
 
-    const group = await SG.findById(groupId);
-    if (!group) {
-        return socket.emit("errorMessage", {
-            message: "Group not found",
-            statusCode: 404
-        });
-    }
+   
+        // check group exists
+        const group = await SG.findById(groupId)
+        if (!group) {
+            return socket.emit("errorMessage", {
+                message: "Group not found",
+                statusCode: 404
+            })
+        }
 
-    const alreadyMember = await GM.findOne({
-        groupId,
-        usersId: socket.userId
-    });
-    if (alreadyMember) {
-        return socket.emit("errorMessage", {
-            message: "You are already a member of this group",
-            statusCode: 400
-        });
-    }
+        // check not already a member
+        const alreadyMember = await GM.findOne({
+            groupId,
+            usersId: socket.userId
+        })
+        if (alreadyMember) {
+            return socket.emit("errorMessage", {
+                message: "You are already a member of this group",
+                statusCode: 400
+            })
+        }
 
-    const alreadyRequested = await JoinRequest.findOne({
-        groupId,
-        userId: socket.userId,
-        status: "pending"
-    });
-    if (alreadyRequested) {
-        return socket.emit("errorMessage", {
-            message: "You already have a pending request",
-            statusCode: 400
-        });
-    }
+        // check not already requested
+        const alreadyRequested = await JoinRequest.findOne({
+            groupId,
+            userId: socket.userId,
+            status: "pending"
+        })
+        if (alreadyRequested) {
+            return socket.emit("errorMessage", {
+                message: "You already have a pending request",
+                statusCode: 400
+            })
+        }
 
-    const joinRequest = await JoinRequest.create({
-        groupId,
-        userId: socket.userId
-    });
+        // create request in DB
+        const joinRequest = await JoinRequest.create({
+            groupId,
+            userId: socket.userId
+        })
 
-    socket.emit("joinRequestSent", {
-        message: "Your request has been sent",
-        data: joinRequest
-    });
+        // notify the user
+        socket.emit("joinRequestSent", {
+            message: "Your request has been sent",
+            data: joinRequest
+        })
 
-    socket.to(group.adminId.toString()).emit("newJoinRequest", {
-        message: "New join request",
-        data: joinRequest
-    });
-};
+        // notify the admin
+        socket.to(group.adminId.toString()).emit("newJoinRequest", {
+            message: "New join request",
+            data: joinRequest
+        })
+
+    
+}
